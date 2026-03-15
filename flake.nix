d70cf846bb60d1357f4527be641341ec5f4653d5
akhyarazamta <akhyar.azamta@gmail.com>
2026-03-15 12:15:11 +0700

    feat: Add Nix flake for Mikhmon development environment and a script to run the PHP server.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description: "A flake to run Mikhmon on NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        php = pkgs.php82.buildEnv {
+          extensions = ({ enabled, all }: enabled ++ [ all.session all.gd all.curl all.mbstring ]);
+          extraConfig = ''
+            memory_limit = 256M
+            post_max_size = 64M
+            upload_max_filesize = 64M
+          '';
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            php
+          ];
+
+          shellHook = ''
+            echo "Mikhmon development environment loaded!"
+            echo "Run ./run.sh to start the server."
+          '';
+        };
+      }
+    );
+}
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Start PHP built-in server
+echo "Starting Mikhmon server at http://localhost:8080..."
+php -S localhost:8080 index.php
